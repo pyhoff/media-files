dbe1ae6c976217c3a8c5f12e71402aa961a4fe4b
pyhoff <pyhoff@gmail.com>
2026-05-10 16:30:46 -0400

    Win hotfix

--- go/console_windows.go
+++ go/console_windows.go
@@ -5,24 +5,31 @@ import (
 	"syscall"
 )
 
-// attachConsole allocates a console when the binary is launched from a terminal
-// in --cli mode. Without this, -H windowsgui suppresses all stdout/stderr output.
+const (
+	stdOutputHandle = uintptr(0xFFFFFFF5) // (DWORD)-11
+	stdErrorHandle  = uintptr(0xFFFFFFF4) // (DWORD)-12
+)
+
 func attachConsole() {
-	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	kernel32         := syscall.NewLazyDLL("kernel32.dll")
 	attachConsoleProc := kernel32.NewProc("AttachConsole")
-	allocConsoleProc := kernel32.NewProc("AllocConsole")
+	allocConsoleProc  := kernel32.NewProc("AllocConsole")
+	setStdHandle      := kernel32.NewProc("SetStdHandle")
 
 	// ATTACH_PARENT_PROCESS = 0xFFFFFFFF
-	ret, _, _ := attachConsoleProc.Call(0xFFFFFFFF)
+	ret, _, _ := attachConsoleProc.Call(^uintptr(0))
 	if ret == 0 {
-		// No parent console (double-clicked from Explorer), allocate a new one
+		// No parent console (e.g. launched from Explorer), open a new one
 		allocConsoleProc.Call()
 	}
 
-	// Reopen stdout/stderr so fmt.Print and os.Stderr work
-	stdout, _ := syscall.Open("CONOUT$", syscall.O_RDWR, 0)
-	syscall.SetStdHandle(syscall.STD_OUTPUT_HANDLE, stdout)
-	syscall.SetStdHandle(syscall.STD_ERROR_HANDLE, stdout)
-	os.Stdout = os.NewFile(uintptr(stdout), "CONOUT$")
-	os.Stderr = os.NewFile(uintptr(stdout), "CONOUT$")
+	conout, err := os.OpenFile("CONOUT$", os.O_RDWR, 0)
+	if err != nil {
+		return
+	}
+
+	setStdHandle.Call(stdOutputHandle, conout.Fd())
+	setStdHandle.Call(stdErrorHandle, conout.Fd())
+	os.Stdout = conout
+	os.Stderr = conout
 }
